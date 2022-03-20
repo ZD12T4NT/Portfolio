68f7d3baf164f94d415f166784a65b21ae5128e9
ZD12T4NT <zacdistant@gmail.com>
2022-03-20 10:49:30 +0000

    change made

--- JS/app.js
+++ JS/app.js
@@ -28,3 +28,37 @@ function reveal () {
     }
 }
 
+
+
+// Scroll to the top function 
+const showOnPx = 100;
+const backToTopButton = document.querySelector("#myBtn");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth"
+  });
+};
+
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
